65c9737f3a57e3fd86eee545791ea4c1fc5ecd77
Sunita Ramnarinesingh <sramnari@gmail.com>
2017-01-26 15:47:16 -0500

    Set up handle bar files

--- models/burger.js
+++ models/burger.js
@@ -2,7 +2,8 @@
 var orm = require('../config/orm.js');
 
 // create the code that will call the ORM functions using burger specific input for the ORM.
-var burger = {
+var burger = 
+{
 
   selectAll: function(callback){
     orm.selectAll(function(res){
